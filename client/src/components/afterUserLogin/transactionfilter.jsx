58d77a1a6d93e1152f50a30bffdf2ea6e657b01e
Natasha Astia <natashaastia@mail.ugm.ac.id>
2022-11-04 08:01:43 +0700

    Add styling for transaction filter page

--- client/src/components/afterUserLogin/transactionfilter.jsx
+++ client/src/components/afterUserLogin/transactionfilter.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import UserCircle from "../../assets/UserCircle.svg";
 import PlusCircle from "../../assets/PlusCircle.svg";
 import FadersWhite from "../../assets/FadersWhite.svg";
+//import Faders from "../../assets/Faders.svg";
+import CaretDown from "../../assets/CaretDown.svg";
 
 const transactionfilter = () => {
   return (
@@ -24,8 +26,8 @@ const transactionfilter = () => {
 
       <div className="h-screen flex font-Roboto pt-44 px-16 pb-10 text-xl">
         <div className="bg-[#D9D9D9] text-black font-black w-full pr-5 rounded-lg">
-          <div className="flex flex-wrap justify-between ml-5 mt-5">
-            <div className="ml-10">
+          <div className="flex justify-between ml-5 mt-5">
+            <div className="ml-2">
               Transactions
             </div>
             <div className="flex justify-between">
@@ -37,6 +39,30 @@ const transactionfilter = () => {
                   <img src={FadersWhite} alt="FadersWhite"/>
                   <div className="ml-2">Filter</div>
               </button>
+            </div>
+          </div>
+          <div className="flex justify-between ml-5 mt">
+            <div className="ml-2 flex justify-between bg-[#319C69] p-1.5 rounded-lg w-full">
+              <button className="items-center text-[#FFFFFF] flex font-normal text-base rounded-lg px-2 py-1">
+                  <img src={FadersWhite} alt="FadersWhite"/>
+                  <div className="ml-2 mr-10">Filter</div>
+              </button>
+              <button className="items-center border border-[#FFFFFF] bg-[#FFFFFF] text-[#686868] flex justify-between font-normal text-base rounded-lg ml-2 px-2 py-1 w-1/5">
+                  <div className="mr-5">By Category</div>
+                  <img src={CaretDown} alt="CaretDown"/>
+              </button>
+              <button className="items-center border border-[#FFFFFF] bg-[#FFFFFF] text-[#686868] flex justify-between font-normal text-base rounded-lg ml-2 px-2 py-1 w-1/5">
+                  <div className="mr-5">By Labels</div>
+                  <img src={CaretDown} alt="CaretDown"/>
+              </button>
+              <button className="items-center border border-[#FFFFFF] bg-[#FFFFFF] text-[#686868] flex justify-between font-normal text-base rounded-lg ml-2 px-2 py-1 w-1/5">
+                  <div className="mr-5">By Date Range</div>
+                  <img src={CaretDown} alt="CaretDown"/>
+              </button>
+              <input
+                className="text-[#000000] font-normal text-base rounded-lg ml-2 px-2 py-1 focus:outline-none w-2/5"
+                placeholder="By Description"
+              />
             </div>
           </div>
         </div>
