cd8afc7e2256c2c762cfa173db016821acc1629b
FarhanMaulanaF <farhanmaulanafirmansyah@mail.ugm.ac.id>
2022-11-17 10:41:40 +0700

    responsive homepage

--- client/src/components/home/homepage.jsx
+++ client/src/components/home/homepage.jsx
@@ -6,11 +6,11 @@ function Homepage() {
     <div className="bg-[#319C69] h-screen font-Roboto">
       {/* navbar  */}
       <div className="text-white flex justify-between items-center">
-        <div className="font-black text-4xl ml-32 my-10">
+        <div className="font-black text-4xl ml-10 md:ml-32 my-10">
           <a href="/">Pristin</a>
         </div>
         <div className="mr-32 my-10">
-          <ul className="flex font-semibold text-lg space-x-14">
+          <ul className="hidden md:flex font-semibold text-lg space-x-14">
             <li>
               <a href="/" className="hover:text-[#263238]">Home</a>
             </li>
@@ -35,21 +35,23 @@ function Homepage() {
       </div>
 
       {/* Pristin Content */}
-      <div id="section-1" className="flex justify-between items-center pt-12">
-        <div>
-          <div className="text-white text-6xl font-black ml-32">Pristin</div>
+      <div id="section-1" className="flex flex-col md:flex-row  md:flex justify-between items-center pt-12">
+        <div className="order-last md:order-first">
+          <div className="text-white text-6xl font-black text-center mt-10 md:ml-32 md:text-left">Pristin</div>
 
-          <div className="text-white text-xl ml-32 font-light my-5">
+          <div className="text-white text-xl text-center md:ml-32 md:text-left font-light my-5">
             Track, note, and manage your daily transactions <br /> all in a
             single page.
           </div>
 
-          <button class="bg-[#F3F3F3] ml-32 font-medium py-3 px-7 rounded-lg my-16 text-xl hover:bg-[#263238] hover:text-white">
-            Get Started
-          </button>
+          <div className="flex justify-center md:justify-start">
+            <button class="bg-[#F3F3F3] justify-center md:text-left md:ml-32 font-medium py-3 px-7 rounded-lg my-16 text-xl hover:bg-[#263238] hover:text-white">
+              Get Started
+            </button>
+          </div>
         </div>
 
-        <div className="mr-32 my-32 scale-110">
+        <div className="flex order-first md:order-last justify-center md:justify-start md:mr-32 md:my-32 md:scale-110">
           <img src={Illustration} alt="illustration" />
         </div>
       </div>
