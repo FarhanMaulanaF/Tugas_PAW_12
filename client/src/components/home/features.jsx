3fd447e9ef6478297cbab38e260a08cfa6be5030
Natasha Astia <natashaastia@mail.ugm.ac.id>
2022-11-17 20:19:00 +0700

    Add styling for feature homepage

--- client/src/components/home/features.jsx
+++ client/src/components/home/features.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import Notes from "../../assets/Notes.svg";
+import Group from "../../assets/Group.svg";
+import Savingmoney from "../../assets/Savingmoney.svg";
+import Visualdata from "../../assets/Visualdata.svg";
 
 function Features() {
   return (
@@ -6,10 +10,38 @@ function Features() {
       <div className="flex flex-col h-fit md:h-screen mx-auto">
         <div className='text-black font-black text-4xl text-center mt-20 md:mt-0 content-center'>Features</div>
         <div className='md:flex flex-row items-center text-center gap-6 md:gap-5 h-4/5 mt-10 md:mt-20 font-medium'>
-          <div className='h-full my-2  mx-auto w-11/12 bg-[#D9D9D9] flex items-center'><p className="mx-auto w-3/5">Record your daily income and expenses</p></div>
-          <div className='h-full my-2 mx-auto w-11/12 bg-[#D9D9D9] flex items-center'><p className="mx-auto w-3/5">Keep track of where all your money is spent</p></div>
-          <div className='h-full my-2 mx-auto w-11/12 bg-[#D9D9D9] flex items-center'><p className="mx-auto w-3/5">Visualize your inclome and expenses based on your own categories</p></div>
-          <div className='h-full my-2 mx-auto w-11/12 bg-[#D9D9D9] flex items-center'><p className="mx-auto w-3/5">Manage your budgeting for future expense</p></div>
+          <div className='h-full my-2 flex flex-col w-11/12 bg-[#319C69] flex items-center rounded-lg'>
+            <div className="m-10">
+              <img src={Notes} alt="Record Income and Expenses Illustration"/>
+            </div>
+            <p className="mx-auto w-3/5 text-[#FFFFFF]">
+            Record your daily income and expenses
+            </p>
+          </div>
+          <div className='h-full my-2 flex flex-col mx-auto w-11/12 bg-[#319C69] flex items-center rounded-lg'>
+            <div className="m-10">
+              <img src={Group} alt="Money Expenses Organization Illustration"/>
+            </div>
+            <p className="mx-auto w-3/5 text-[#FFFFFF]">
+              Keep track of where all your money is spent
+            </p>
+          </div>
+          <div className='h-full my-2 mx-auto flex flex-col w-11/12 bg-[#319C69] flex items-center rounded-lg'>
+            <div className='mt-10'>
+              <img src={Savingmoney} alt="Saving Money Illustration"/>
+            </div>
+            <p className="mx-auto w-3/5 text-[#FFFFFF] mt-10">
+              Visualize your income and expenses based on your own categories
+            </p>
+          </div>
+          <div className='h-full my-2 mx-auto flex flex-col w-11/12 bg-[#319C69] flex items-center'>
+            <div className='m-10'>
+              <img src={Visualdata} alt="Saving Money Illustration"/>
+            </div>
+            <p className="mx-auto w-3/5 text-[#FFFFFF]">
+              Manage your budgeting for future expense
+            </p>
+          </div>
         </div>
       </div>
     </div>
