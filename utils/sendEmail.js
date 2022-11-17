49b4f220d52cb0fbfee75299c2e296eb6c59cd81
Hervi Nur Rahmandien <71449881+hervibest@users.noreply.github.com>
2022-11-17 20:16:17 +0700

    Major Change in dashboard

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,52 +1,48 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
+const sendEmail = (options, callback) => {
+  var transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
 
-const sendEmail = (options,callback) =>
-{
-    var transporter = nodemailer.createTransport({
-      host : process.env.SMTP_HOST,
-      port : process.env.SMTP_PORT,
-      auth:{
-          user: process.env.EMAIL_USERNAME,
-          pass: process.env.EMAIL_PASSWORD
-      }
-  })
+  // const handlebarOptions = {
+  //   viewEngine: {
+  //     extName: ".handlebars",
+  //     partialsDir: path.resolve('./views'),
+  //     defaultLayout: false,
+  //   },
+  //   viewPath: path.resolve('./views'),
+  //   extName: ".handlebars",
+  // }
 
-      
-      // const handlebarOptions = {
-      //   viewEngine: {
-      //     extName: ".handlebars",
-      //     partialsDir: path.resolve('./views'),
-      //     defaultLayout: false,
-      //   },
-      //   viewPath: path.resolve('./views'),
-      //   extName: ".handlebars",
-      // }
-      
-      //transporter.use('compile', hbs(handlebarOptions));
-      
-      var mailOptions = {
-        from: process.env.EMAIL_FROM,
-        to: options.to,
-        subject: options.subject,
-        html: options.text
-        // context: {
-        //   name: options.name,
-        //   title: options.title,
-        //   text: options.text,
-        //   text2: options.text2,
-        // }
-      
-      };
-      
-      transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-          console.log("Email gagal dikirim" + error);
-          callback(false);
-        } else {
-          console.log('Email sent: ' + info.response);
-          callback(true);
-        }
-      });
+  //transporter.use('compile', hbs(handlebarOptions));
+
+  var mailOptions = {
+    from: process.env.EMAIL_FROM,
+    to: options.to,
+    subject: options.subject,
+    html: options.text,
+    // context: {
+    //   name: options.name,
+    //   title: options.title,
+    //   text: options.text,
+    //   text2: options.text2,
+    // }
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log("Email gagal dikirim" + error);
+      callback(false);
+    } else {
+      console.log("Email sent: " + info.response);
+      callback(true);
     }
+  });
+};
 module.exports = sendEmail;
