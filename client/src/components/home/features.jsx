76ace3778ba7a1bbf1944f4c8bba2c19ceeea010
FarhanMaulanaF <farhanmaulanafirmansyah@mail.ugm.ac.id>
2022-11-17 12:54:51 +0700

    responsive features page

--- client/src/components/home/features.jsx
+++ client/src/components/home/features.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 function Features() {
   return (
-    <div id="section-2" name='features' className="bg-white h-screen py-32 flex flex-col font-Roboto">
-      <div className="flex flex-col item-center h-full mx-auto">
-        <div className=' text-black font-black text-4xl text-center  content-center'>Features</div>
-        <div className='flex text-center gap-5 items-center h-4/5 mt-20 font-medium'>
-          <div className='h-full w-1/2 bg-[#D9D9D9] flex items-center justify-center'><p>Record your daily income and expenses</p></div>
-          <div className='h-full w-1/2 bg-[#D9D9D9] flex items-center justify-center'>Keep track of where all your money is spent</div>
-          <div className='h-full w-1/2 bg-[#D9D9D9] flex items-center justify-center'>Visualize your inclome and expenses based on your own categories</div>
-          <div className='h-full w-1/2 bg-[#D9D9D9] flex items-center justify-center'>Manage your budgeting for future expense</div>
+    <div id="section-2" name='features' className="bg-white h-screen md:py-32 flex flex-col font-Roboto">
+      <div className="flex flex-col h-fit md:h-screen mx-auto">
+        <div className='text-black font-black text-4xl text-center mt-20 md:mt-0 content-center'>Features</div>
+        <div className='md:flex flex-row items-center text-center gap-6 md:gap-5 h-4/5 mt-10 md:mt-20 font-medium'>
+          <div className='h-full my-2  mx-auto w-11/12 bg-[#D9D9D9] flex items-center'><p className="mx-auto w-3/5">Record your daily income and expenses</p></div>
+          <div className='h-full my-2 mx-auto w-11/12 bg-[#D9D9D9] flex items-center'><p className="mx-auto w-3/5">Keep track of where all your money is spent</p></div>
+          <div className='h-full my-2 mx-auto w-11/12 bg-[#D9D9D9] flex items-center'><p className="mx-auto w-3/5">Visualize your inclome and expenses based on your own categories</p></div>
+          <div className='h-full my-2 mx-auto w-11/12 bg-[#D9D9D9] flex items-center'><p className="mx-auto w-3/5">Manage your budgeting for future expense</p></div>
         </div>
       </div>
     </div>
